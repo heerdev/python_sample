578d056bccd9462a1c45ecf04f6e745f93080dd0
zuahmed <zubair.ahmed@gmail.com>
2026-01-30 17:30:33 +0100

    vscode

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,12 +1,12 @@
 from pydantic_settings import BaseSettings
-
+import os
 
 class Settings(BaseSettings):
     app_name: str = "SampleApp"
     env: str = "dev"
 
     class Config:
-        env_file = ".env"
+        env_file = f".env.{os.getenv('PROFILE', 'dev')}"
 
 
 settings = Settings()
