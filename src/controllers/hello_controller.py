7d626adfdc42f5611c433c605d3b8df5965fbc63
zuahmed <zubair.ahmed@gmail.com>
2026-01-31 18:11:28 +0100

    new brach

--- src/controllers/hello_controller.py
+++ src/controllers/hello_controller.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, Depends
+import matplotlib.pyplot as plt
+import networkx as nx
 from src.domain.service import GreetingService
 
 router = APIRouter()
@@ -8,4 +10,8 @@ def get_greeting_service():
 
 @router.get("/hello")
 def hello(service: GreetingService = Depends(get_greeting_service)):
-    return {"message": service.greet("Alice")}
+      G = nx.Graph()
+      G.add_edges_from([(1,2),(2,3),(3,1)])
+      nx.draw(G, with_labels=True)
+      plt.show()
+      return {"message": service.greet("Alice")}
